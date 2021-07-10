12121f362c17cb037b359b769260ca66620b7a84
Asyraf <are.syraf97@yahoo.com>
2021-07-10 22:30:51 +0800

    1st commit

---
+++ android/app/src/main/kotlin/com/example/personal_expenses_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.personal_expenses_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'Data 1', amount: 69.99, date: DateTime.now()),
+    Transaction(id: 't1', title: 'Data 2', amount: 16.53, date: DateTime.now()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Widget Playground!'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            ),
+          ]),
+    );
+  }
+}
